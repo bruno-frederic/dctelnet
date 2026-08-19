c2e3cc5482f1f619a1bacd8494fcc7a6a91c2612
Bruno FREDERIC <6505880+bruno-frederic@users.noreply.github.com>
2026-08-19 21:49:53 +0200

    chore: bump version to 1.9.1-dev

--- src/DCTelnet.c
+++ src/DCTelnet.c
@@ -3,7 +3,7 @@
 /* ====================================================================== */
 
 
-#define DCTELNET_VERSION "1.9"
+#define DCTELNET_VERSION "1.9.1-dev"
 const char __ver[] = "$VER: DCTelnet " DCTELNET_VERSION " " __AMIGADATE__;
 
 #ifndef BUILD_HASH
